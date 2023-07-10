967c0a951ba88c1f9b9a0272022532bfd9659fa7
Farmeurimmo <farmeurimmo@gmail.com>
2023-07-10 14:22:39 +0200

    Content update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -32,6 +32,7 @@ export default function Contact() {
         <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
             <Navigation/>
             <title>Contact - Reaper Solutions</title>
+            <h1 className="text-4xl font-bold text-center text-white font-display md:text-6xl">Contact</h1>
             <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
                 <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
                     {socials.map((s) => (
--- app/reapersanction/page.tsx
+++ app/reapersanction/page.tsx
@@ -5,6 +5,8 @@ import {Navigation} from "../components/nav";
 import {Card} from "../components/card";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faDiscord, faGithub} from '@fortawesome/free-brands-svg-icons'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {tomorrow} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const good_points = [
     "Gui punishment plugin for spigot minecraft server.",
@@ -14,6 +16,33 @@ const good_points = [
     "Customizable punishments",
     "Customizable GUIs",
 ]
+
+const yamGuiConfig = `MAIN: # <-- This is the main gui
+  name: §4ReaperSanction # <-- The name of the gui
+  size: 27 # <-- The size of the gui (9, 18, 27, 36, 45, 54)
+  # Slots start at 0 and end at size - 1,
+  # If you place an item in a slot outside of the gui size, it can cause errors
+  isFill: true
+  # If isFill is true, the gui will be filled with a glass pane
+  # The option to customize the glass pane will be added in the future
+  items: # <-- The items of the gui
+    '8': # <-- The slot of the item
+      type: ANVIL # <-- The type of the item
+      amount: 1 # <-- The amount of the item
+      display: §6What§cEver§1You§bWant # <-- The display name of the item
+      lore: # <-- The lore of the item
+        '0': Hi # <-- The line of the lore, you have to start at 0, incrementing by 1 and write like it is
+        '1': Custom lore
+        '2': With custom actions
+      actions: # <-- The actions of the item
+        '0': EXT->SAY HELLO %player%  # <- Similar to lore, you have to start at 0, incrementing by 1
+        # More explaination about how and what is available below.
+    '16': # Here is another item
+      type: PAPER
+      amount: 1
+      display: §cEnd
+      actions:
+        '0': INT->GUI->END->%player%`;
 
 const links = [
     {
@@ -40,7 +69,7 @@ const links = [
 
 export default function Dev() {
     return (
-        <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+        <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 overflow-hidden">
             <Navigation/>
             <Particles
                 className="absolute inset-0 -z-10 animate-fade-in"
@@ -84,8 +113,8 @@ export default function Dev() {
                         </ul>
                     </div>
                     <div className="mt-16"/>
-                    <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">Screenshots</h1>
-                    <p className="mt-4 text-sm text-zinc-400">Some screenshots of ReaperSanction</p>
+                    <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl md:text-4xl">Screenshots</h2>
+                    <p className="mt-4 text-sm text-zinc-400">Some screenshots of the plugin.</p>
                     <div className="flex flex-col items-center justify-center w-full mx-auto text-center mt-4 gap-6">
                         <div
                             className="flex flex-row items-center justify-center w-full mx-auto text-center mt-4 gap-6">
@@ -103,9 +132,13 @@ export default function Dev() {
                             </Card>
                         </div>
                     </div>
+                    <div className="mt-16"/>
+                    <div
+                        className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"/>
                     <div className="mt-24"/>
                     <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">Wiki</h1>
-                    <p className="mt-4 text-sm text-zinc-400">The wiki of ReaperSanction</p>
+                    <p className="mt-4 text-sm text-zinc-400">The wiki of ReaperSanction.</p>
+                    <div className="mt-12"/>
                     <div className="flex flex-col items-center justify-center w-full mx-auto text-center mt-4 gap-6">
                         <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl md:text-4xl">Installation</h2>
                         <div className="flex flex-col items-center justify-center w-full mx-auto text-left mt-4 gap-6">
@@ -119,11 +152,10 @@ export default function Dev() {
                         <div className="mt-16"/>
                         <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl md:text-4xl">Commands</h2>
                         <p>() = optional, [] = required</p>
-                        <table className="table-auto text-zinc-300">
+                        <table id="table" className="table-auto text-zinc-300">
                             <thead>
                             <tr>
                                 <th className="px-4 py-2">Command</th>
-                                <th className="px-4 py-2">Alias</th>
                                 <th className="px-4 py-2">Usage</th>
                                 <th className="px-4 py-2">Permission</th>
                                 <th className="px-4 py-2">Description</th>
@@ -131,7 +163,6 @@ export default function Dev() {
                             </thead>
                             <tbody>
                             <tr>
-                                <td className="border px-4 py-2">/reapersanction</td>
                                 <td className="border px-4 py-2">/rs</td>
                                 <td className="border px-4 py-2">/rs [player]</td>
                                 <td className="border px-4 py-2">reapersanction</td>
@@ -139,14 +170,12 @@ export default function Dev() {
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/ban</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/ban [player] (reason)</td>
                                 <td className="border px-4 py-2">reapersanction.ban</td>
                                 <td className="border px-4 py-2">Ban a player (permanent)</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/tempban</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/tempban [player] [time with sec/min/hour/day/year]
                                     (reason)
                                 </td>
@@ -155,28 +184,24 @@ export default function Dev() {
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/unban</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/unban [player]</td>
                                 <td className="border px-4 py-2">reapersanction.unban</td>
                                 <td className="border px-4 py-2">Unban a player</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/kick</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/kick [player] (reason)</td>
                                 <td className="border px-4 py-2">reapersanction.kick</td>
                                 <td className="border px-4 py-2">Kick a player</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/mute</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/mute [player] (reason)</td>
                                 <td className="border px-4 py-2">reapersanction.mute</td>
                                 <td className="border px-4 py-2">Mute a player (permanent)</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/tempmute</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/tempmute [player] [time with sec/min/hour/day/year]
                                     (reason)
                                 </td>
@@ -185,42 +210,36 @@ export default function Dev() {
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/unmute</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/unmute [player]</td>
                                 <td className="border px-4 py-2">reapersanction.unmute</td>
                                 <td className="border px-4 py-2">Unmute a player</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/ban-ip</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/ban-ip [ip or player (preferred)] (reason)</td>
                                 <td className="border px-4 py-2">reapersanction.banip</td>
                                 <td className="border px-4 py-2">Ban an ip and a player (permanent)</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/vanish</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/vanish (player)</td>
                                 <td className="border px-4 py-2">reapersanction.vanish</td>
                                 <td className="border px-4 py-2">Vanish yourself or a player</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/report</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/report [player]</td>
                                 <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">Report a player via gui</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/history</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/history [player]</td>
                                 <td className="border px-4 py-2">reapersanction.history</td>
                                 <td className="border px-4 py-2">See the history of a player</td>
                             </tr>
                             <tr>
                                 <td className="border px-4 py-2">/rsadmin</td>
-                                <td className="border px-4 py-2"></td>
                                 <td className="border px-4 py-2">/rsadmin [rl/reload/infos/migratedb]</td>
                                 <td className="border px-4 py-2">reapersanction.admin</td>
                                 <td className="border px-4 py-2">Reload the config, get the plugin infos, get
@@ -229,6 +248,62 @@ export default function Dev() {
                             </tr>
                             </tbody>
                         </table>
+                        <div className="mt-24"/>
+                        <h2 className="text-2xl font-bold">Custom GUIs</h2>
+                        <p>See below for the list of compatible GUIs with their exact name in config.</p>
+                        <div className="text-left">
+                            <ul className="list-disc list-inside">
+                                <li>Main GUI -> MAIN</li>
+                                <li>Mute GUI -> MUTE</li>
+                                <li>Ban GUI -> BAN</li>
+                                <li>History GUI -> HISTORY</li>
+                                <li>Report GUI -> REPORT</li>
+                                <li>End GUI -> END</li>
+                                <li>Ban IP GUI -> BAN_IP</li>
+                            </ul>
+                        </div>
+                        <div className="mt-12"/>
+                        <h3 className="text-xl font-bold">How the GUI/Message config work</h3>
+                        <p className="text-left">The config is red at the start of the server and when you use the
+                            command /rsadmin rl. You can edit the config and use the command to reload it. The config
+                            may not order the GUIs in the same order as you want. You can change the order of the GUIs,
+                            this won't affect the plugin.
+                        </p>
+                        <h4 className="text-lg font-bold">Location</h4>
+                        <p className="text-left">The path of the GUI config is :
+                            plugins/ReaperSanction/Inventories.yml</p>
+                        <p className="text-left">The path of the message config is :
+                            plugins/ReaperSanction/Messages.yml</p>
+                        <div className="mt-6"/>
+                        <h4 className="text-lg font-bold">GUI config</h4>
+                        <div id="code" className="overflow-x-auto">
+                            <SyntaxHighlighter language="yaml" style={tomorrow}>
+                                {yamGuiConfig}
+                            </SyntaxHighlighter>
+                        </div>
+                        <div className="mt-6"/>
+                        <h4 className="text-lg font-bold">Actions</h4>
+                        <h5 className="text-lg font-bold">EXT actions</h5>
+                        <p className="text-left">The EXT actions will sudo the player to execute the command.
+                            For example if we have "EXT->SAY HELLO %player%", the player will execute :
+                            "/say HELLO %player%". %player% represent the player name.
+                        </p>
+                        <div className="mt-6"/>
+                        <h5 className="text-lg font-bold">INT actions</h5>
+                        <p className="text-left">The INT actions will transfer instructions to the plugin.
+                            For example if we have "INT->TEMPBAN->%player%->7day->Fly", the plugin will tempban the
+                            player for 7 days for "Fly".
+                        </p>
+                        <p className="text-left">The INT actions are have optional arguments. For example we can remove
+                            the reason of the action. If we have "INT->TEMPBAN->%player%->7day", the plugin will tempban
+                            the player for 7 days with the default reason.
+                        </p>
+                        <div className="mt-6"/>
+                        <h5 className="text-lg font-bold">INT actions ENDPOINTS</h5>
+                        <p className="text-left">The INT actions ENDPOINTS are the possible actions that the plugin can
+                            do. The list of the possible actions is below.
+                        </p>
+                        <p>IN REDACTION</p>
                     </div>
                     <div className="mt-24"/>
                 </div>
